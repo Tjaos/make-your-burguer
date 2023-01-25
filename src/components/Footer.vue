5fb59b9f946769852f62b7218ae907d302c3ce60
tjaos <Thiago>
2023-01-25 13:15:23 -0300

    parte do front feita: footer, navbar, icon, router-link

---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="footer">
+        <p>Make Your Burguer &copy; 2023</p>
+    </div>
+</template>
+
+<script>
+export default{
+    name: "Footers"
+}
+</script>
+
+<style scoped>
+#footer{
+    height: 150px;
+    background-color: #222;
+    border-top: 4px solid #111;
+    color: #FCBA03;
+    display: flex;
+    align-items:center;
+    justify-content:center;
+    width: 100%;
+    margin-top: 96px;
+    
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,49 @@
+<template>
+    <div id="nav">
+      <router-link to="/" id="logo-url">
+          <img :src="logo" :alt="alt" id="logo">
+      </router-link>
+      <router-link to="/">Home</router-link>
+      <router-link to="/Pedidos">Pedidos</router-link> 
+    </div>
+</template>
+<script>
+export default{
+    name: "Navbar",
+    props: ["logo", "alt"]
+}
+
+</script>
+  
+  <style scoped>
+  /*Div main */
+    #nav{
+      background-color: #222;
+      border-bottom: 4px solid #111;
+      padding: 15px 50px;
+      display: flex;
+      justify-content: flex-end;
+      align-items: center;
+    }
+    /*position component-router logo */
+    #nav #logo-url{
+      margin: auto;
+      margin-left: 0;
+    }
+    /*Logo Style */
+    #logo{
+      width: 40px;
+      height: 40px;
+    }
+    /*links decoration*/
+    #nav a{
+      color: #FCBA03;
+      text-decoration: none;
+      margin: 12px;
+      transition: .4s;
+    }
+    #nav a:hover{
+      color: #FFF;
+    }
+  </style>
+  
