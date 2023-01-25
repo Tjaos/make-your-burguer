b5aeb078c13a5f02b643737615fa7dd818792e71
tjaos <Thiago>
2023-01-25 15:54:50 -0300

    otimização no style do front

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,19 @@
 <template>
+  <Banner/>
   <div class="main-container">
-    <h1>HOME</h1>
+    <h1>Monte o seu burguer:</h1>
+    <BurguerForm/>
   </div>
 </template>
 
 <script>
-
+import Banner from "../components/banner.vue"
+import BurguerForm from "../components/BurguerForm.vue"
+export default{
+  name: "Home",
+  components:{
+    Banner,
+    BurguerForm,
+  }
+}
 </script>
